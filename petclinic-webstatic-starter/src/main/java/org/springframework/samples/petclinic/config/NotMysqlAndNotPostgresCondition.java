3331e1ade9a2683f12174949ec3faa7d7ba549a7
svs-devel <example@example.com>
2024-01-15 20:23:50 +0300

    petclinic-webstatic-starter: add 'appDatasource' bean depends on profile (mysql, postgres)

---
+++ petclinic-webstatic-starter/src/main/java/org/springframework/samples/petclinic/config/NotMysqlAndNotPostgresCondition.java
@@ -0,0 +1,18 @@
+package org.springframework.samples.petclinic.config;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+/**
+ * @author vssavin on 13.01.2024.
+ */
+class NotMysqlAndNotPostgresCondition implements Condition {
+	@Override
+	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+		Environment environment = context.getEnvironment();
+		return !environment.acceptsProfiles(Profiles.of("mysql", "postgres"));
+	}
+}
--- petclinic-webstatic-starter/src/main/java/org/springframework/samples/petclinic/config/PetclinicDataSourcesConfig.java
+++ petclinic-webstatic-starter/src/main/java/org/springframework/samples/petclinic/config/PetclinicDataSourcesConfig.java
@@ -1,6 +1,8 @@
 package org.springframework.samples.petclinic.config;
 
 import com.github.vssavin.usmancore.config.UsmanDataSourceConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.*;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
@@ -17,8 +19,18 @@ public class PetclinicDataSourcesConfig {
 
 	private DataSource appDataSource;
 
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Value("${spring.datasource.username}")
+	private String dbUserName;
+
+	@Value("${spring.datasource.password}")
+	private String dbPassword;
+
 	@Bean
 	@Primary
+	@Conditional(NotMysqlAndNotPostgresCondition.class)
 	public DataSource appDatasource() {
 		if (appDataSource == null) {
 			appDataSource = new EmbeddedDatabaseBuilder().generateUniqueName(true)
@@ -28,6 +40,30 @@ public class PetclinicDataSourcesConfig {
 				.build();
 		}
 		return appDataSource;
+	}
+
+	@Bean("appDatasource")
+	@Profile("mysql")
+	@Primary
+	public DataSource mysqlDatasource() {
+		DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+		dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+		dataSourceBuilder.url(dbUrl);
+		dataSourceBuilder.username(dbUserName);
+		dataSourceBuilder.password(dbPassword);
+		return dataSourceBuilder.build();
+	}
+
+	@Bean("appDatasource")
+	@Profile("postgres")
+	@Primary
+	public DataSource postgresqlDatasource() {
+		DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+		dataSourceBuilder.driverClassName("org.postgresql.Driver");
+		dataSourceBuilder.url(dbUrl);
+		dataSourceBuilder.username(dbUserName);
+		dataSourceBuilder.password(dbPassword);
+		return dataSourceBuilder.build();
 	}
 
 }
