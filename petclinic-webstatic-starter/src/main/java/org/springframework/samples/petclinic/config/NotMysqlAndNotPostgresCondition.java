72d348bb3b82dcebf87ade6a40a5f2518c6c660a
svs-devel <example@example.com>
2024-01-15 20:25:03 +0300

    petclinic-webstatic-starter: fix java-format

--- petclinic-webstatic-starter/src/main/java/org/springframework/samples/petclinic/config/NotMysqlAndNotPostgresCondition.java
+++ petclinic-webstatic-starter/src/main/java/org/springframework/samples/petclinic/config/NotMysqlAndNotPostgresCondition.java
@@ -10,9 +10,11 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
  * @author vssavin on 13.01.2024.
  */
 class NotMysqlAndNotPostgresCondition implements Condition {
+
 	@Override
 	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
 		Environment environment = context.getEnvironment();
 		return !environment.acceptsProfiles(Profiles.of("mysql", "postgres"));
 	}
+
 }
